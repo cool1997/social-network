3553688192570c0694359f27eb1b45169d6482ab
cool1997 <kirilov.vl.ol@gmail.com>
2020-08-29 13:01:29 +0300

    Updates

---
+++ precache-manifest.28d80a5f547c0d18edd0d19ebd74ad94.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c01ca8edd3cde34c703e31e56c9dba3",
+    "url": "/cool1997/social-network/index.html"
+  },
+  {
+    "revision": "960abfe0e69ecd802712",
+    "url": "/cool1997/social-network/static/css/4.a0b39b22.chunk.css"
+  },
+  {
+    "revision": "97dbf0cd369eac57da3c",
+    "url": "/cool1997/social-network/static/css/5.9fe9bae5.chunk.css"
+  },
+  {
+    "revision": "3860eea5e880bd657772",
+    "url": "/cool1997/social-network/static/css/6.c31f7c1f.chunk.css"
+  },
+  {
+    "revision": "389eb06250b372abee82",
+    "url": "/cool1997/social-network/static/css/main.ce3b3fa0.chunk.css"
+  },
+  {
+    "revision": "e3717970d2c003b97f69",
+    "url": "/cool1997/social-network/static/js/2.c6387ffe.chunk.js"
+  },
+  {
+    "revision": "cec4dc19707e3647ff0b46bcc9936b37",
+    "url": "/cool1997/social-network/static/js/2.c6387ffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fab521e0ea1d36f33d2",
+    "url": "/cool1997/social-network/static/js/3.76af8bf1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cool1997/social-network/static/js/3.76af8bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "960abfe0e69ecd802712",
+    "url": "/cool1997/social-network/static/js/4.784d57ad.chunk.js"
+  },
+  {
+    "revision": "97dbf0cd369eac57da3c",
+    "url": "/cool1997/social-network/static/js/5.8fa86b5e.chunk.js"
+  },
+  {
+    "revision": "3860eea5e880bd657772",
+    "url": "/cool1997/social-network/static/js/6.4db1a8ee.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/cool1997/social-network/static/js/6.4db1a8ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "389eb06250b372abee82",
+    "url": "/cool1997/social-network/static/js/main.ead9ec36.chunk.js"
+  },
+  {
+    "revision": "8822ed9e7774609fe21f",
+    "url": "/cool1997/social-network/static/js/runtime-main.77d99578.js"
+  },
+  {
+    "revision": "7869e84bdda526c2724a8d16fb6af33d",
+    "url": "/cool1997/social-network/static/media/preloader.7869e84b.svg"
+  }
+]);
